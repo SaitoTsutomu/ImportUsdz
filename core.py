77fc0bf691cc5d593441ba0b8c6f329fd49228b5
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-08-25 22:31:13 +0900

    upd

--- core.py
+++ core.py
@@ -1,5 +1,10 @@
+import os.path
+import zipfile
+from pathlib import Path
+
 import bpy
 from bpy.props import FloatProperty
+from more_itertools import first, last
 
 from .register_class import _get_cls
 
@@ -15,7 +20,23 @@ class CIU_OT_import_usdz(bpy.types.Operator):
     scale: FloatProperty() = FloatProperty(default=1)  # type: ignore
 
     def execute(self, context):
-
+        # Downloadsの最新usdzファイルを取得
+        gl = Path(os.path.expanduser("~/Downloads")).glob("*.usdz")
+        file = last(sorted((fnam.stat().st_mtime, fnam) for fnam in gl), (0, ""))[-1]
+        if not file:
+            self.report({"INFO"}, "No files.")
+            return {"CANCELLED"}
+        tmpdir = "/tmp/impusdz"
+        os.makedirs(tmpdir, exist_ok=True)
+        # tmpdirに解凍
+        with zipfile.ZipFile(file) as zf:
+            zf.extractall(tmpdir)
+        usdc = first(Path(tmpdir).glob("*.usdc"), "")
+        if not usdc:
+            self.report({"INFO"}, "No usdc.")
+            return {"CANCELLED"}
+        # usdcをインポート
+        bpy.ops.wm.usd_import(filepath=str(usdc), scale=self.scale, import_usd_preview=True)
         return {"FINISHED"}
 
 
